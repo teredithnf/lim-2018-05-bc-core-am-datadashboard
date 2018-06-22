22b912076cd8b6ce8160be7b11cb6434710d328f
teredithnf <tere.navarro85@gmail.com>
2018-06-22 12:01:34 -0500

    progress

--- src/data.js
+++ src/data.js
@@ -2,6 +2,57 @@
 
 // Funciones Solicitadas
 window.computeUsersStats = (users, progress, courses) => {
+       // crear objeto stats con valores por default
+   /* let stats = {
+            percent: 0,
+            exercises: {
+                total: 0,
+                completed: 0,
+                percent: 0
+            },
+            reads: {
+                total: 0,
+                completed: 0,
+                percent: 0
+            },
+    
+            quizzes: {
+                total: 0,
+                completed: 0,
+                percent: 0, 
+                scoreSum: 0,
+                scoreAverage: 0
+            }
+        }
+    
+        //recorrer el array de users y a cada uno agregarle el atributo stats
+        let userWithStats= users.map(function(user){ 
+            user.stats = stats;
+    
+            //obtener el progreso del usuario desde el array progress
+            let userProgress = progress[user.id];
+    
+            if(userProgress){
+    
+                // actualizar el objeto stats con los datos del progreso
+                let percent = courses.reduce((suma, course) => {
+                    let userProgressCourses = userProgress[course];
+                    if(userProgressCourses){
+                        return suma = userProgressCourses.percent;
+                    }
+                },0);
+    
+                user.stats.percent = percent;
+    
+                //actualizar exercises, reads y quizzes
+    
+            }*/
+    
+            return user;
+         });
+    
+        return userWithStats;
+     }
     
 }
 
